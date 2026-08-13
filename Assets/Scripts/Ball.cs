4711ff160658d1e3b5538b5694967ee2ccbce3be
Henryrouze <henryrouze@gmail.com>
2026-08-13 21:27:48 +0900

    Implement ball movement and collision detection; normalize initial direction, add collision responses for walls and players, and log goals. Update TagManager with new tags for walls and goals.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,12 +9,43 @@ public class Ball : MonoBehaviour
     {
         float x = Random.Range(0, 2) == 0 ? -1 : 1;
         float y = Random.Range(-1f, 1f);
-        direction = new Vector2(x, y);
+        direction = new Vector2(x, y).normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Top wall"))
+        {
+            direction.y = -direction.y;
+        }
+            if(collision.gameObject.CompareTag("Bottom wall"))
+        {
+            direction.y = -direction.y;
+        }
+            if(collision.gameObject.CompareTag("Player 1"))
+        {
+            direction.x = -direction.x;
+        }
+            if(collision.gameObject.CompareTag("Player 2"))
+        {
+            direction.x = -direction.x;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Goal 1"))
+        {
+            Debug.Log("Goal 1");
+        }
+        if(other.gameObject.CompareTag("Goal 2"))
+        {
+            Debug.Log("Goal 2");
+        }
     }
 }
